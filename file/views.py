607fe0e428e5d2d41a5cb6dbdc187162c056c47c
user <hunchan12@gmail.com>
2018-10-26 16:07:26 +0900

    파일 업로드+ 폴더생성

--- file/views.py
+++ file/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 def upload1(request):
     if request.method == 'GET':
         return render(request, 'file/upload1.html',{})
     else:
         upload_file = request.FILES['my_file']
-        with open('file/'+ upload_file.name , 'wb') as file:
+
+        login_id = request.session['id']
+        os.mkdir(login_id) # 디렉토리 생성
+
+        with open('file/'+ upload_file.name, 'wb') as file:
             for chunk in upload_file.chunks():
                 file.write(chunk)
 
